9401048cf08bb7f105c3abc17425adb099847532
Dang-Nam Vu <dangnam.vu@Administrators-MacBook-Pro.local>
2019-01-14 12:18:56 -0500

    uploaded the app

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = ({ siteTitle }) => (
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/">
+        <img width="30" src={require('../images/logo-designcode.svg')} />
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy">
+        <button>Buy</button>
+      </Link>
+    </div>
+  </div>
+)
+
+export default Header
