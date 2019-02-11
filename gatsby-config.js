71c3482850541badae1aa97c55b2b5425ef29f5d
Dang-Nam Vu <dangnam.vu@pefin.com>
2019-02-11 14:28:42 -0500

    finished graphql/contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'b3t6se30yo46',
+        accessToken: '15fdf36ff570bb3984d5054f3744985e4cbc37082df3f3cc43ff716bea34de28'
+      }
+    }
+  ],
 }
